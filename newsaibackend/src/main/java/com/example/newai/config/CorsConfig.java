8762b5b2baf9bf394b69290deef1628aa5d7a352
OH0LYW4T3R <tjdtnco@gmail.com>
2025-05-20 22:43:09 +0900

    [BUG] - CORS ERROR Test1

--- newsaibackend/src/main/java/com/example/newai/config/CorsConfig.java
+++ newsaibackend/src/main/java/com/example/newai/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         //JWT 쿠키기반 사용
